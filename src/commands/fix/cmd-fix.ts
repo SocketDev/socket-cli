56fdeca04faec7801ea52ef9f2a06dec775a07cb
Peter van der Zee <github-public@qfox.nl>
2025-02-19 15:59:02 +0100

    Seems I forgot to split the fix command

--- src/commands/fix/cmd-fix.ts
+++ src/commands/fix/cmd-fix.ts
@@ -1,14 +1,9 @@
 import meowOrExit from 'meow'
 
-import { Spinner } from '@socketsecurity/registry/lib/spinner'
-
-import constants from '../../constants'
-import { shadowNpmInstall } from '../../utils/npm'
+import { runFix } from './run-fix.ts'
 import { getFlagListOutput } from '../../utils/output-formatting.ts'
 
 import type { CliCommandConfig } from '../../utils/meow-with-subcommands.ts'
-
-const { SOCKET_CLI_IN_FIX_CMD, SOCKET_IPC_HANDSHAKE } = constants
 
 const config: CliCommandConfig = {
   commandName: 'fix',
@@ -42,46 +37,5 @@ async function run(
     flags: config.flags
   })
 
-  // const prev = new Set(alerts.map(a => a.key))
-  // let ret: SafeNode | undefined
-  // /* eslint-disable no-await-in-loop */
-  // while (alerts.length > 0) {
-  //   await updateAdvisoryNodes(this, alerts)
-  //   ret = await this[kRiskyReify](...args)
-  //   await this.loadActual()
-  //   await this.buildIdealTree()
-  //   needInfoOn = getPackagesToQueryFromDiff(this.diff, {
-  //     includeUnchanged: true
-  //   })
-  //   alerts = (
-  //     await getPackagesAlerts(needInfoOn, {
-  //       includeExisting: true,
-  //       includeUnfixable: true
-  //     })
-  //   ).filter(({ key }) => {
-  //     const unseen = !prev.has(key)
-  //     if (unseen) {
-  //       prev.add(key)
-  //     }
-  //     return unseen
-  //   })
-  // }
-  // /* eslint-enable no-await-in-loop */
-  // return ret!
-
-  const spinner = new Spinner().start()
-  try {
-    await shadowNpmInstall({
-      ipc: {
-        [SOCKET_IPC_HANDSHAKE]: {
-          [SOCKET_CLI_IN_FIX_CMD]: true
-        }
-      }
-    })
-  } catch (e: any) {
-    console.error(e)
-    spinner.error()
-  } finally {
-    spinner.stop()
-  }
+  await runFix()
 }
