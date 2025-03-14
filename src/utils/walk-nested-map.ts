955cd01507907882715893c30119af7514f4b093
Peter van der Zee <github-public@qfox.nl>
2025-03-14 13:17:49 +0100

    Add test for the map walker

--- src/utils/walk-nested-map.ts
+++ src/utils/walk-nested-map.ts
@@ -4,11 +4,11 @@ export function* walkNestedMap<T>(
   map: NestedMap<T>,
   keys: string[] = []
 ): Generator<{ keys: string[]; value: T }> {
-  for (const [_key, value] of map.entries()) {
+  for (const [key, value] of map.entries()) {
     if (value instanceof Map) {
-      yield* walkNestedMap(value as NestedMap<T>, keys.concat(_key))
+      yield* walkNestedMap(value as NestedMap<T>, keys.concat(key))
     } else {
-      yield { keys, value: value }
+      yield { keys: keys.concat(key), value: value }
     }
   }
 }
