206d92b956dd7db2f66ae66c5b6a44d424dc8640
jdalton <john.david.dalton@gmail.com>
2025-03-20 11:29:02 -0400

    Use sdk for batchScan

--- src/utils/lockfile/pnpm-lock-yaml.ts
+++ src/utils/lockfile/pnpm-lock-yaml.ts
@@ -1,8 +1,10 @@
 import { detectDepTypes } from '@pnpm/lockfile.detect-dep-types'
 
-import { batchScan } from '../alert/artifact'
-import { AlertsByPkgId, addArtifactToAlertsMap } from '../socket-package-alert'
+import { getPublicToken, setupSdk } from '../sdk'
+import { addArtifactToAlertsMap } from '../socket-package-alert'
 
+import type { CompactSocketArtifact } from '../alert/artifact'
+import type { AlertsByPkgId } from '../socket-package-alert'
 import type { Lockfile } from '@pnpm/lockfile-file'
 import type { Spinner } from '@socketsecurity/registry/lib/spinner'
 
@@ -31,6 +33,7 @@ export async function getAlertsMapFromPnpmLockfile(
 
   const depTypes = detectDepTypes(lockfile)
   const pkgIds = Object.keys(depTypes)
+
   let { length: remaining } = pkgIds
   const alertsByPkgId: AlertsByPkgId = new Map()
   if (!remaining) {
@@ -40,12 +43,29 @@ export async function getAlertsMapFromPnpmLockfile(
 
   spinner?.start(getText())
 
+  const socketSdk = await setupSdk(getPublicToken())
+
   const toAlertsMapOptions = {
     overrides: lockfile.overrides,
     ...options
   }
-  for await (const artifact of batchScan(pkgIds)) {
-    await addArtifactToAlertsMap(artifact, alertsByPkgId, toAlertsMapOptions)
+
+  for await (const batchPackageFetchResult of socketSdk.batchPackageStream(
+    {
+      alerts: 'true',
+      compact: 'true'
+    },
+    {
+      components: pkgIds.map(id => ({ purl: `pkg:npm/${id}` }))
+    }
+  )) {
+    if (batchPackageFetchResult.success) {
+      await addArtifactToAlertsMap(
+        batchPackageFetchResult.data as CompactSocketArtifact,
+        alertsByPkgId,
+        toAlertsMapOptions
+      )
+    }
     remaining -= 1
     if (spinner && remaining > 0) {
       spinner.start()
