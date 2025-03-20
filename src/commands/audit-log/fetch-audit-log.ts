b5ef2aac28cb9be3d7975d95175ed0bc49d13cff
Peter van der Zee <209817+pvdz@users.noreply.github.com>
2025-03-20 16:29:32 +0100

    Apply handle pattern to audit-logs (#373) * Apply handle pattern to audit-logs * Maybe it makes more sense this way

---
+++ src/commands/audit-log/fetch-audit-log.ts
@@ -0,0 +1,81 @@
+import constants from '../../constants'
+import { handleApiCall, handleUnsuccessfulApiResponse } from '../../utils/api'
+import { AuthError } from '../../utils/errors'
+import { getDefaultToken, setupSdk } from '../../utils/sdk'
+
+import type { SocketSdkReturnType } from '@socketsecurity/sdk'
+
+export async function fetchAuditLog({
+  logType,
+  orgSlug,
+  outputKind,
+  page,
+  perPage
+}: {
+  outputKind: 'json' | 'markdown' | 'print'
+  orgSlug: string
+  page: number
+  perPage: number
+  logType: string
+}): Promise<SocketSdkReturnType<'getAuditLogEvents'>['data'] | void> {
+  const apiToken = getDefaultToken()
+  if (!apiToken) {
+    throw new AuthError(
+      'User must be authenticated to run this command. To log in, run the command `socket login` and enter your API key.'
+    )
+  }
+
+  return await fetchAuditLogWithToken(apiToken, {
+    logType,
+    orgSlug,
+    outputKind,
+    page,
+    perPage
+  })
+}
+
+export async function fetchAuditLogWithToken(
+  apiToken: string,
+  {
+    logType,
+    orgSlug,
+    outputKind,
+    page,
+    perPage
+  }: {
+    outputKind: 'json' | 'markdown' | 'print'
+    orgSlug: string
+    page: number
+    perPage: number
+    logType: string
+  }
+): Promise<SocketSdkReturnType<'getAuditLogEvents'>['data'] | void> {
+  // Lazily access constants.spinner.
+  const { spinner } = constants
+
+  spinner.start(`Looking up audit log for ${orgSlug}`)
+
+  const socketSdk = await setupSdk(apiToken)
+  const result = await handleApiCall(
+    socketSdk.getAuditLogEvents(orgSlug, {
+      // I'm not sure this is used at all.
+      outputJson: String(outputKind === 'json'),
+      // I'm not sure this is used at all.
+      outputMarkdown: String(outputKind === 'markdown'),
+      orgSlug,
+      type: logType,
+      page: String(page),
+      per_page: String(perPage)
+    }),
+    `Looking up audit log for ${orgSlug}\n`
+  )
+
+  if (!result.success) {
+    handleUnsuccessfulApiResponse('getAuditLogEvents', result)
+    return
+  }
+
+  spinner.stop()
+
+  return result.data
+}
