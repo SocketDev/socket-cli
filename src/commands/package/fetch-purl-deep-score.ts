1172be334cfc92630b0e17504c21003bfb0ef79d
Peter van der Zee <github-public@qfox.nl>
2025-03-27 11:12:15 +0100

    Improve error handling

--- src/commands/package/fetch-purl-deep-score.ts
+++ src/commands/package/fetch-purl-deep-score.ts
@@ -19,25 +19,39 @@ export async function fetchPurlDeepScore(purl: string) {
   }
 
   spinner.start('Getting deep package score...')
-  const response = await queryApi(
-    `purl/score/${encodeURIComponent(purl)}`,
-    apiToken
-  )
-  spinner?.successAndStop('Received deep package score response.')
-
-  if (!response.ok) {
-    const err = await handleApiError(response.status)
-    logger.log('\nThere was an error', err)
-    spinner.errorAndStop(
-      `${colors.bgRed(colors.white(response.statusText))}: ${err}`
-    )
+  let result
+  try {
+    result = await queryApi(`purl/score/${encodeURIComponent(purl)}`, apiToken)
+    spinner?.successAndStop('Received deep package score response.')
+  } catch (e) {
+    spinner?.failAndStop('The request was unsuccessful.')
+    const msg = (e as undefined | { message: string })?.message
+    if (msg) {
+      logger.fail(msg)
+      logger.log(
+        'Please report this if the error persists or use the cli version that includes error reporting to automate that'
+      )
+    } else {
+      logger.log(
+        'An error happened but no reason was given. If this persists please let us know about it and what you were trying to achieve. Thank you.'
+      )
+    }
     return
   }
 
-  const result = await handleApiCall(await response.text(), 'Reading text')
+  if (!result.ok) {
+    const err = await handleApiError(result.status)
+    logger.fail(
+      `${colors.bgRed(colors.bold(colors.white(' ' + result.statusText + ' ')))}: ${err}`
+    )
+    process.exitCode = 1
+    return
+  }
+
+  const data = await handleApiCall(await result.text(), 'Reading text')
 
   try {
-    return JSON.parse(result)
+    return JSON.parse(data)
   } catch (e) {
     throw new Error(
       'Was unable to JSON parse the input from the server. It may not have been a proper JSON response. Please report this problem.'
