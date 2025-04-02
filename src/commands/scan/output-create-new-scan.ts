1c5dae8f39de4850c6a3352d7fab6048de4c1b27
jdalton <john.david.dalton@gmail.com>
2025-04-02 14:37:57 -0400

    Use more prompts

--- src/commands/scan/output-create-new-scan.ts
+++ src/commands/scan/output-create-new-scan.ts
@@ -1,10 +1,8 @@
-import process from 'node:process'
-import readline from 'node:readline/promises'
-
 import open from 'open'
 import colors from 'yoctocolors-cjs'
 
 import { logger } from '@socketsecurity/registry/lib/logger'
+import { confirm } from '@socketsecurity/registry/lib/prompts'
 
 import type { SocketSdkReturnType } from '@socketsecurity/sdk'
 
@@ -14,17 +12,12 @@ export async function outputCreateNewScan(
   const link = colors.underline(colors.cyan(`${data.html_report_url}`))
   logger.log(`Available at: ${link}`)
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  const answer = await rl.question(
-    'Would you like to open it in your browser? (y/n)'
-  )
-
-  if (answer.toLowerCase() === 'y') {
+  if (
+    await confirm({
+      message: 'Would you like to open it in your browser?',
+      default: false
+    })
+  ) {
     await open(`${data.html_report_url}`)
   }
-  rl.close()
 }
