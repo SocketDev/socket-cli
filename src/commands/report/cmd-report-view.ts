16ee7455866fd2f9102377ac8744ed0bb548e7d3
Peter van der Zee <github-public@qfox.nl>
2025-04-03 09:56:21 +0200

    Sunset the report command

--- src/commands/report/cmd-report-view.ts
+++ src/commands/report/cmd-report-view.ts
@@ -1,14 +1,10 @@
 import { logger } from '@socketsecurity/registry/lib/logger'
 
-import { viewReport } from './view-report'
 import constants from '../../constants'
 import { commonFlags, outputFlags, validationFlags } from '../../flags'
-import { handleBadInput } from '../../utils/handle-bad-input'
 import { meowOrExit } from '../../utils/meow-with-subcommands'
 
 import type { CliCommandConfig } from '../../utils/meow-with-subcommands'
-
-const { DRY_RUN_BAIL_TEXT } = constants
 
 const config: CliCommandConfig = {
   commandName: 'view',
@@ -36,51 +32,14 @@ async function run(
   importMeta: ImportMeta,
   { parentName }: { parentName: string }
 ): Promise<void> {
-  const cli = meowOrExit({
+  meowOrExit({
     argv,
     config,
     importMeta,
     parentName
   })
 
-  const { json, markdown } = cli.flags
-  const [reportId = '', ...extraInput] = cli.input
-
-  const wasBadInput = handleBadInput(
-    {
-      test: reportId,
-      message: 'Need at least one report ID',
-      pass: 'ok',
-      fail: 'missing'
-    },
-    {
-      nook: true,
-      test: extraInput.length === 0,
-      message: 'Can only handle a single report ID',
-      pass: 'ok',
-      fail: 'received ' + (extraInput.length + 1)
-    },
-    {
-      nook: true,
-      test: !json || !markdown,
-      message: 'The json and markdown flags cannot be both set, pick one',
-      pass: 'ok',
-      fail: 'omit one'
-    }
+  logger.fail(
+    'This command has been sunset. Instead, please look at `socket scan create` to create scans and `socket scan report` to view a report of your scans.'
   )
-  if (wasBadInput) {
-    return
-  }
-
-  if (cli.flags['dryRun']) {
-    logger.log(DRY_RUN_BAIL_TEXT)
-    return
-  }
-
-  await viewReport(reportId, {
-    all: Boolean(cli.flags['all']),
-    commandName: `${parentName} ${config.commandName}`,
-    outputKind: json ? 'json' : markdown ? 'markdown' : 'print',
-    strict: Boolean(cli.flags['strict'])
-  })
 }
