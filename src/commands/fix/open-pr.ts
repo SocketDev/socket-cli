820691053687cd79a199301ef3aefda9728ebb11
jdalton <john.david.dalton@gmail.com>
2025-04-15 19:23:58 -0600

    Remove more debug

--- src/commands/fix/open-pr.ts
+++ src/commands/fix/open-pr.ts
@@ -63,7 +63,6 @@ export async function doesPullRequestExistForBranch(
       head: `${owner}:${branch}`,
       state: 'open'
     })
-    debugLog('doesPullRequestExistForBranch > prs', prs)
     return prs.length > 0
   } catch {}
   return false
