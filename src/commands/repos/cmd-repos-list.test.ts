46c95fc00e4814cdcfb0457c8f77d7001a7b78dd
Peter van der Zee <209817+pvdz@users.noreply.github.com>
2025-04-23 01:27:39 +0200

    Repos commands in v1 mode (#468)
    * Minor cleanup
    
    * Start adding a flagged v1 flow; repos create
    
    * Save time running bs
    
    * lies
    
    * nodis
    
    * oh a string you say ok
    
    * Okay add the other repos commands too
    
    ---------
    
    Co-authored-by: jdalton <john.david.dalton@gmail.com>
    Co-authored-by: John-David Dalton <jdalton@users.noreply.github.com>

--- src/commands/repos/cmd-repos-list.test.ts
+++ src/commands/repos/cmd-repos-list.test.ts
@@ -30,14 +30,16 @@ describe('socket repos list', async () => {
           Options
             --direction       Direction option
             --help            Print this help
+            --interactive     Allow for interactive elements, asking for input. Use --no-interactive to prevent any input questions, defaulting them to cancel/no.
             --json            Output result as json
             --markdown        Output result as markdown
+            --org             Force override the organization slug, overrides the default org from config
             --page            Page number
             --perPage         Number of results per page
             --sort            Sorting option
 
           Examples
-            $ socket repos list FakeOrg"
+            $ socket repos list <org slug>"
       `
       )
       expect(`\n   ${stderr}`).toMatchInlineSnapshot(`
@@ -70,7 +72,7 @@ describe('socket repos list', async () => {
 
         \\x1b[31m\\xd7\\x1b[39m \\x1b[41m\\x1b[1m\\x1b[37m Input error: \\x1b[39m\\x1b[22m\\x1b[49m \\x1b[1mPlease review the input requirements and try again\\x1b[22m
 
-          - Org name as the first argument (\\x1b[31mmissing\\x1b[39m)
+          - Org name must be the first argument (\\x1b[31mmissing\\x1b[39m)
 
           - You need to be logged in to use this command. See \`socket login\`. (\\x1b[31mmissing API token\\x1b[39m)"
       `)
@@ -91,6 +93,92 @@ describe('socket repos list', async () => {
           |   __|___ ___| |_ ___| |_      | Socket.dev CLI ver <redacted>
           |__   | * |  _| '_| -_|  _|     | Node: <redacted>, API token set: <redacted>
           |_____|___|___|_,_|___|_|.dev   | Command: \`socket repos list\`, cwd: <redacted>"
+      `)
+
+      expect(code, 'dry-run should exit with code 0 if input ok').toBe(0)
+    }
+  )
+
+  cmdit(
+    [
+      'repos',
+      'list',
+      '--dry-run',
+      '--config',
+      '{"isTestingV1": true, "apiToken":"anything"}'
+    ],
+    'should report missing org name in v1',
+    async cmd => {
+      const { code, stderr, stdout } = await invokeNpm(entryPath, cmd)
+      expect(stdout).toMatchInlineSnapshot(`""`)
+      expect(`\n   ${stderr}`).toMatchInlineSnapshot(`
+        "
+           _____         _       _        /---------------
+          |   __|___ ___| |_ ___| |_      | Socket.dev CLI ver <redacted> (is testing v1)
+          |__   | * |  _| '_| -_|  _|     | Node: <redacted>, API token set: <redacted>
+          |_____|___|___|_,_|___|_|.dev   | Command: \`socket repos list\`, cwd: <redacted>
+        \\x1b[32m   (Thank you for testing the v1 bump! Please send us any feedback you might have!)
+        \\x1b[39m
+        Missing the org slug and no --org flag set. Trying to auto-discover the org now...
+        Note: you can set the default org slug to prevent this issue. You can also override all that with the --org flag.
+        \\x1b[31m\\xd7\\x1b[39m Skipping auto-discovery of org in dry-run mode
+        \\x1b[31m\\xd7\\x1b[39m \\x1b[41m\\x1b[1m\\x1b[37m Input error: \\x1b[39m\\x1b[22m\\x1b[49m \\x1b[1mPlease review the input requirements and try again\\x1b[22m
+
+          - Org name by default setting, --org, or auto-discovered (\\x1b[31mmissing\\x1b[39m)"
+      `)
+
+      expect(code, 'dry-run should exit with code 2 if missing input').toBe(2)
+    }
+  )
+
+  cmdit(
+    [
+      'repos',
+      'list',
+      '--dry-run',
+      '--config',
+      '{"isTestingV1": true, "apiToken":"anything", "defaultOrg": "fakeorg"}'
+    ],
+    'should accept default org in v1',
+    async cmd => {
+      const { code, stderr, stdout } = await invokeNpm(entryPath, cmd)
+      expect(stdout).toMatchInlineSnapshot(`"[DryRun]: Bailing now"`)
+      expect(`\n   ${stderr}`).toMatchInlineSnapshot(`
+        "
+           _____         _       _        /---------------
+          |   __|___ ___| |_ ___| |_      | Socket.dev CLI ver <redacted> (is testing v1)
+          |__   | * |  _| '_| -_|  _|     | Node: <redacted>, API token set: <redacted>, default org: <redacted>
+          |_____|___|___|_,_|___|_|.dev   | Command: \`socket repos list\`, cwd: <redacted>
+        \\x1b[32m   (Thank you for testing the v1 bump! Please send us any feedback you might have!)
+        \\x1b[39m"
+      `)
+
+      expect(code, 'dry-run should exit with code 0 if input ok').toBe(0)
+    }
+  )
+
+  cmdit(
+    [
+      'repos',
+      'list',
+      '--org',
+      'forcedorg',
+      '--dry-run',
+      '--config',
+      '{"isTestingV1": true, "apiToken":"anything"}'
+    ],
+    'should accept --org flag in v1',
+    async cmd => {
+      const { code, stderr, stdout } = await invokeNpm(entryPath, cmd)
+      expect(stdout).toMatchInlineSnapshot(`"[DryRun]: Bailing now"`)
+      expect(`\n   ${stderr}`).toMatchInlineSnapshot(`
+        "
+           _____         _       _        /---------------
+          |   __|___ ___| |_ ___| |_      | Socket.dev CLI ver <redacted> (is testing v1)
+          |__   | * |  _| '_| -_|  _|     | Node: <redacted>, API token set: <redacted>
+          |_____|___|___|_,_|___|_|.dev   | Command: \`socket repos list\`, cwd: <redacted>
+        \\x1b[32m   (Thank you for testing the v1 bump! Please send us any feedback you might have!)
+        \\x1b[39m"
       `)
 
       expect(code, 'dry-run should exit with code 0 if input ok').toBe(0)
