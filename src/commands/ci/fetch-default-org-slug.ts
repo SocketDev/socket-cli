a86942bcd5143548d6be814e91cc9f7e4a290dae
Peter van der Zee <github-public@qfox.nl>
2025-04-25 11:06:38 +0200

    Update the auto org fetcher for ci command to be json friendly

--- src/commands/ci/fetch-default-org-slug.ts
+++ src/commands/ci/fetch-default-org-slug.ts
@@ -1,11 +1,13 @@
+import { debugLog } from '@socketsecurity/registry/lib/debug'
 import { logger } from '@socketsecurity/registry/lib/logger'
 
+import { CliJsonResult } from '../../types'
 import { handleApiCall } from '../../utils/api'
 import { getConfigValue } from '../../utils/config'
 import { setupSdk } from '../../utils/sdk'
 
 // Use the config defaultOrg when set, otherwise discover from remote
-export async function getDefaultOrgSlug(): Promise<string | void> {
+export async function getDefaultOrgSlug(): Promise<CliJsonResult<string>> {
   let defaultOrg = getConfigValue('defaultOrg')
   if (defaultOrg) {
     logger.info(`Using default org: ${defaultOrg}`)
@@ -18,38 +20,42 @@ export async function getDefaultOrgSlug(): Promise<string | void> {
     // Ignore a failed request here. It was not the primary goal of
     // running this command and reporting it only leads to end-user confusion.
     if (!result.success) {
-      logger.fail(
-        'Failed to fetch default organization from API. Unable to continue.'
-      )
       process.exitCode = 1
-      return
+      return {
+        ok: false,
+        message: result.error,
+        data: `Failed to fetch default organization from API. Unable to continue.${result.cause ? ` ( Reason given: ${result.cause} )` : ''}`
+      }
     }
+
     const orgs = result.data.organizations
     const keys = Object.keys(orgs)
 
     if (!keys[0]) {
-      logger.fail(
-        'Could not find default organization for the current API token. Unable to continue.'
-      )
       process.exitCode = 1
-      return
+      return {
+        ok: false,
+        message: 'Failed to establish identity',
+        data: `API did not return any organization associated with the current API token. Unable to continue.`
+      }
     }
 
     const slug = (keys[0] in orgs && orgs?.[keys[0]]?.name) ?? undefined
 
     if (slug) {
       defaultOrg = slug
-      logger.info(`Resolved org to: ${defaultOrg}`)
+      debugLog(`Resolved org to: ${defaultOrg}`)
     }
   }
 
   if (!defaultOrg) {
-    logger.fail(
-      'Could not find the default organization for the current API token. Unable to continue.'
-    )
     process.exitCode = 1
-    return
+    return {
+      ok: false,
+      message: 'Failed to establish identity',
+      data: `Was unable to determine the default organization for the current API token. Unable to continue.`
+    }
   }
 
-  return defaultOrg
+  return { ok: true, data: defaultOrg }
 }
