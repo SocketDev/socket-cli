b21a80506cd080dcb46418ee083d4d8af5d1513d
John-David Dalton <jdalton@users.noreply.github.com>
2025-04-27 09:02:54 -0700

    Add erasableSyntaxOnly ts config (#479)

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,3 +1,7 @@
+export function createEnum<const T extends Record<string, any>>(obj: T): Readonly<T> {
+  return Object.freeze({ __proto__: null, ...obj }) as any
+}
+
 export function pick<T extends Record<string, any>, K extends keyof T>(
   input: T,
   keys: K[] | readonly K[]
