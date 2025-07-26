4cad6c3cd800fc18aa245b78a4b8915916ca0422
jdalton <john.david.dalton@gmail.com>
2025-07-25 22:04:08 -0400

    Fix shadow-npm-bin name references

--- bin/npm-cli.js
+++ bin/npm-cli.js
@@ -6,5 +6,5 @@ const path = require('node:path')
 const rootPath = path.join(__dirname, '..')
 Module.enableCompileCache?.(path.join(rootPath, '.cache'))
 
-const shadowBin = require(path.join(rootPath, 'dist/shadow-bin.js'))
+const shadowBin = require(path.join(rootPath, 'dist/shadow-npm-bin.js'))
 shadowBin('npm')
