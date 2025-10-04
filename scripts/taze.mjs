ddc96131a5a180d181f39b7c6046c0808956d20c
jdalton <john.david.dalton@gmail.com>
2025-10-04 00:37:18 -0400

    Format taze.mjs script

--- scripts/taze.mjs
+++ scripts/taze.mjs
@@ -22,10 +22,14 @@ void (async () => {
   // Run with command line arguments.
   const args = process.argv.slice(2)
 
-  const tazePromise = spawn('pnpm', ['taze', '-c', '.config/taze.config.mts', ...args], {
-    stdio: 'pipe',
-    cwd: process.cwd(),
-  })
+  const tazePromise = spawn(
+    'pnpm',
+    ['taze', '-c', '.config/taze.config.mts', ...args],
+    {
+      stdio: 'pipe',
+      cwd: process.cwd(),
+    },
+  )
 
   let hasProvenanceDowngrade = false
 
