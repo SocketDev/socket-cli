dddd8bb1c9565aed5fc5c5282686a1a551d977bd
jdalton <john.david.dalton@gmail.com>
2025-10-06 01:19:19 -0400

    Enable inline-require-calls babel plugin
    Configure babel to use the inline-require-calls plugin for build optimization.
    This allows marking specific require() calls with @__INLINE__ for inlining at build time.

--- .config/babel.config.js
+++ .config/babel.config.js
@@ -21,6 +21,7 @@ module.exports = {
         version: '^7.27.1',
       },
     ],
+    path.join(babelPluginsPath, 'babel-plugin-inline-require-calls.js'),
     path.join(babelPluginsPath, 'transform-set-proto-plugin.mjs'),
     path.join(babelPluginsPath, 'transform-url-parse-plugin.mjs'),
   ],
