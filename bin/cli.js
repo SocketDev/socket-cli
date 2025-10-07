e4d432397eaf02a02e856f344e1cc48fa751f7bb
jdalton <john.david.dalton@gmail.com>
2025-10-07 06:08:38 -0400

    feat: add pkg binary detection to bin/cli.js
    Adds detection for pkg/yao-pkg binaries via process.pkg check. When running as
    a pkg binary, directly executes the CLI instead of spawning a new process, since
    pkg binaries cannot spawn themselves with custom Node flags.
    
    This enables Socket CLI to work when packaged as standalone executables with
    yao-pkg for binary distribution.

--- bin/cli.js
+++ bin/cli.js
@@ -10,46 +10,62 @@ void (async () => {
   const { default: constants } = require(
     path.join(rootPath, 'dist/constants.js'),
   )
-  const { spawn } = require(
-    path.join(
-      rootPath,
-      'dist/external/@socketsecurity/registry/dist/lib/spawn.js',
-    ),
-  )
 
-  process.exitCode = 1
+  // Detect if running as pkg/yao-pkg binary
+  const isPkg = typeof process.pkg !== 'undefined'
 
-  const spawnPromise = spawn(
-    constants.execPath,
-    [
-      ...constants.nodeNoWarningsFlags,
-      ...constants.nodeDebugFlags,
-      ...constants.nodeHardenFlags,
-      ...constants.nodeMemoryFlags,
-      ...(constants.ENV.INLINED_SOCKET_CLI_SENTRY_BUILD
-        ? ['--require', constants.instrumentWithSentryPath]
-        : []),
-      constants.distCliPath,
-      ...process.argv.slice(2),
-    ],
-    {
-      env: {
-        ...process.env,
-        ...constants.processEnv,
+  if (isPkg) {
+    // Running as pkg binary - directly execute CLI without spawning
+    process.exitCode = 1
+
+    // Set environment variables
+    Object.assign(process.env, constants.processEnv)
+
+    // Directly require and execute the CLI
+    require(constants.distCliPath)
+  } else {
+    // Running as normal Node - use spawn with flags
+    const { spawn } = require(
+      path.join(
+        rootPath,
+        'dist/external/@socketsecurity/registry/dist/lib/spawn.js',
+      ),
+    )
+
+    process.exitCode = 1
+
+    const spawnPromise = spawn(
+      constants.execPath,
+      [
+        ...constants.nodeNoWarningsFlags,
+        ...constants.nodeDebugFlags,
+        ...constants.nodeHardenFlags,
+        ...constants.nodeMemoryFlags,
+        ...(constants.ENV.INLINED_SOCKET_CLI_SENTRY_BUILD
+          ? ['--require', constants.instrumentWithSentryPath]
+          : []),
+        constants.distCliPath,
+        ...process.argv.slice(2),
+      ],
+      {
+        env: {
+          ...process.env,
+          ...constants.processEnv,
+        },
+        stdio: 'inherit',
       },
-      stdio: 'inherit',
-    },
-  )
+    )
 
-  // See https://nodejs.org/api/child_process.html#event-exit.
-  spawnPromise.process.on('exit', (code, signalName) => {
-    if (signalName) {
-      process.kill(process.pid, signalName)
-    } else if (typeof code === 'number') {
-      // eslint-disable-next-line n/no-process-exit
-      process.exit(code)
-    }
-  })
+    // See https://nodejs.org/api/child_process.html#event-exit.
+    spawnPromise.process.on('exit', (code, signalName) => {
+      if (signalName) {
+        process.kill(process.pid, signalName)
+      } else if (typeof code === 'number') {
+        // eslint-disable-next-line n/no-process-exit
+        process.exit(code)
+      }
+    })
 
-  await spawnPromise
+    await spawnPromise
+  }
 })()
