047ee0745031cdf741f31b8a7d348af0baafc3d6
jdalton <john.david.dalton@gmail.com>
2025-10-15 23:48:28 -0400

    remove placeholders and patches

--- packages/placeholders/cli-win32-arm64/bin/cli.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Socket CLI placeholder for win32-arm64
-echo Please wait for the real package to be published.
-echo Visit https://github.com/SocketDev/socket-cli for more info.
-exit /b 1
